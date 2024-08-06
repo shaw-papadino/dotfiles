c0dfd59c1c9b87a75235227dbac65db91702386d
dino <sho.edu.human@gmail.com>
2024-08-06 14:36:59 +0900

    fix setup.sh

--- _link.sh
+++ _link.sh
@@ -6,3 +6,4 @@ for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
 done
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-#!bin/bash
+#!/bin/bash
+
+# TARGET=(all, work, hobby)
+TARGET=${1:-"all"}
 
 echo "Xcodeをインストールします..."
 xcode-select --install
@@ -12,19 +15,27 @@ if ! type brew > /dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo "brew docterを実行します..."
+# echo "brew docterを実行します..."
 brew doctor
 
-echo "brew updateを実行します..."
+# echo "brew updateを実行します..."
 brew update
 
-echo "brew upgradeを実行します..."
+# echo "brew upgradeを実行します..."
 brew upgrade
 
-echo ".Brewfileで管理しているパッケージをインストールします..."
-brew bundle --file=./.Brewfile
+echo ".Brewfileで管理しているパッケージをインストールします... TARGET: $TARGET"
+brew bundle --file=./.Brewfile_common
+if [ "$TARGET" = "work" ]; then
+    brew bundle --file=./.Brewfile_work
+elif [ "$TARGET" = "hobby" ]; then
+    brew bundle --file=./.Brewfile_hobby
+else
+    brew bundle --file=./.Brewfile_work
+    brew bundle --file=./.Brewfile_hobby
+fi
 
-echo "brew cleanupを実行します..."
+# echo "brew cleanupを実行します..."
 brew cleanup
 
 #----------------------------------------
