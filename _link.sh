0e755e381e89e7ca0ba6d027be62be4f5f98e217
dino <sho.edu.human@gmail.com>
2025-02-15 21:05:34 +0900

    add gitignore_global

--- _link.sh
+++ _link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # シンボリックリンクの作成
-DOT_FILES=".zshrc .vimrc .gitconfig .ideavimrc"
+DOT_FILES=".zshrc .vimrc .gitconfig .ideavimrc .gitignore_global"
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
